62cedf143c2a6726b692d89c35be9bf1b2e6c956
serhatbek <serhat.bekk@gmail.com>
2024-03-13 23:55:07 +0300

    tutorial setup

---
+++ src/tutorial.ts
@@ -0,0 +1,13 @@
+console.log('typescript tutorial');
+
+interface someValue {
+  name: string;
+  id: number;
+}
+
+const someObject: someValue = {
+  name: 'Serhat',
+  id: 6,
+};
+
+console.log(someObject);
