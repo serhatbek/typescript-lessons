99d5d2674ecdb6e1dfaf9a31a580c5feeb102abf
serhatbek <serhat.bekk@gmail.com>
2024-03-17 18:16:07 +0300

    Objects

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,4 +1,23 @@
 /* -------------------------------------------------------------------------- */
+// SECTION - Objects
+let book = { title: 'book', cost: 20 };
+let pen = { title: 'pen', cost: 18 };
+let notebook = { title: 'notebook' };
+
+// let items: { title: string; cost: number }[] = [book, pen, notebook]; //NOTE - Property 'cost' is missing in type '{ title: string; }' but required in type '{ title: string; cost: number; }'.ts(2741)
+let deskItems: { title: string; cost?: number }[] = [book, pen, notebook];
+console.log(deskItems);
+
+let bike: { brand: string; year: number } = { brand: 'climax', year: 2014 };
+// bike.year = '2014' //NOTE - Type 'string' is not assignable to type 'number'.
+let laptop: { brand: string; year: number } = { brand: 'MSI', year: 2024 };
+// let laptop2: {brand:string; year:number} = {brand:'MAC'}  // Property 'year' is missing in type '{ brand: string; }' but required in type '{ brand: string; year: number; }'.ts(2741)
+let products: { title: string; price?: number }[] = [
+  { title: 'phone', price: 44 },
+  { title: 'tv' },
+];
+
+/* -------------------------------------------------------------------------- */
 // SECTION - Arrays
 let temperatures: number[] = [20, 18, 41];
 temperatures.push(38);
@@ -59,31 +78,31 @@ console.log(notSure('Bedirhan'));
 /* -------------------------------------------------------------------------- */
 //SECTION - Union Type
 
-// let tax: number | string = 20;
-// tax = '6';
+let tax: number | string = 20;
+tax = '6';
 
-// console.log(tax);
-// let requestStatus: 'pending' | 'error' | 'success' = 'pending';
+console.log(tax);
+let requestStatus: 'pending' | 'error' | 'success' = 'pending';
 
-// requestStatus = 'error';
-// requestStatus = 'success';
-// requestStatus = 'random';
+requestStatus = 'error';
+requestStatus = 'success';
+// requestStatus = 'random';  // Type '"random"' is not assignable to type '"pending" | "error" | "success"'.
 
-// console.log(requestStatus);
+console.log(requestStatus);
 
 /* -------------------------------------------------------------------------- */
 
 //SECTION - Type Annotations
 
-// let animal: string = 'Cat';
-// animal.concat(', loves milk.');
+let animal: string = 'Cat';
+animal.concat(', loves milk.');
 
-// let water: boolean = true;
-// water.valueOf();
+let water: boolean = true;
+water.valueOf();
 
-// let num: number = 6;
-// num.toExponential();
+let num: number = 6;
+num.toExponential();
 
-// console.log(animal);
-// console.log(water);
-// console.log(num);
+console.log(animal);
+console.log(water);
+console.log(num);
