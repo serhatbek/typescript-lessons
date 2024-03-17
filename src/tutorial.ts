4ee13fc682e0c6466caeb30092ca0caebbf46b5c
serhatbek <serhat.bekk@gmail.com>
2024-03-17 16:26:57 +0300

    Union Type

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,12 +1,26 @@
-let animal: string = 'Cat';
-animal.concat(', loves milk.');
+let tax: number | string = 20;
+tax = '6';
 
-let water: boolean = true;
-water.valueOf();
+console.log(tax);
+let requestStatus: 'pending' | 'error' | 'success' = 'pending';
 
-let num: number = 6;
-num.toExponential();
+requestStatus = 'error';
+requestStatus = 'success';
+// requestStatus = 'random';
 
-console.log(animal);
-console.log(water);
-console.log(num);
+console.log(requestStatus);
+
+//-------------------------
+
+// let animal: string = 'Cat';
+// animal.concat(', loves milk.');
+
+// let water: boolean = true;
+// water.valueOf();
+
+// let num: number = 6;
+// num.toExponential();
+
+// console.log(animal);
+// console.log(water);
+// console.log(num);
