56dd0e0c2fb1bbef1df832d92e3c94e932d7492e
serhatbek <serhat.bekk@gmail.com>
2024-04-15 23:35:42 +0300

    Tasks Project - Add Task

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,4 +1,3 @@
-const addTaskBtn = document.querySelector<HTMLButtonElement>('.btn');
 const taskInput = document.querySelector<HTMLInputElement>('.form-input');
 const taskForm = document.querySelector<HTMLFormElement>('.form');
 const tasksListContainer = document.querySelector<HTMLUListElement>('.list');
@@ -19,10 +18,24 @@ taskForm?.addEventListener('submit', (event) => {
   // get input value
   let taskValue = taskInput?.value;
   if (taskValue) {
-    console.log(taskValue);
+    const task: Task = {
+      description: taskValue,
+      isCompleted: false,
+    };
+    // add task to list
+    addTask(task);
+    console.log(tasks);
+    // render tasks
+
+    // update local storage
+
     // if value clean everything and return
     taskInput.value = '';
     return;
   }
   alert('Please add a task description');
 });
+
+const addTask = (task: Task): void => {
+  tasks.push(task);
+};
