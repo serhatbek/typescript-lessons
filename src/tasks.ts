ad9d22fa5b5995879f498b88d973690b02772062
serhatbek <serhat.bekk@gmail.com>
2024-04-15 16:24:38 +0300

    Tasks Project - Useful Info

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,1 +1,12 @@
-console.log('Serhat 🩷🩷🩷 cats');
+// const btn = document.querySelector<HTMLButtonElement>('.test-btn')!;
+// btn.disabled = true;
+
+// const btn = document.querySelector<HTMLButtonElement>('.test-btn');
+// if (btn) {
+//   btn.disabled = true;
+// }
+
+const btn = document.querySelector('.test-btn')! as HTMLButtonElement; // type assertion
+if (btn) {
+  btn.disabled = true;
+}
